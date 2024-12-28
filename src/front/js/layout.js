5382217643a1b6cb3e2fac804605f962bb982d3e
MauSon1971 <110358138+MauSon1971@users.noreply.github.com>
2024-12-28 09:05:28 +0000

    03.- Arreglo de Bugs en models/reservas y limpieza de plantilla

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -4,8 +4,6 @@ import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
 
 import { Home } from "./pages/home";
-import { Demo } from "./pages/demo";
-import { Single } from "./pages/single";
 import { Login } from "./pages/login";
 import injectContext from "./store/appContext";
 
