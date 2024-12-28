47706219d697e8d6aae3f8ad991f3c350b616b46
MauSon1971 <110358138+MauSon1971@users.noreply.github.com>
2024-12-28 09:20:07 +0000

    Revert "Merge pull request #10 from 4GeeksAcademy/login"
    This reverts commit a39e27d8188f0950885fa7aeb2f1412eefc42068, reversing
    changes made to aeee85843e524bd7d8c810510806cf6e11862101.

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User, Empresa, Alojamientos, MediosPago, Preguntas, Transacciones, QRDatabase, Reservas, GuardiaCivil
+from .models import db, User
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -12,14 +12,6 @@ def setup_admin(app):
     
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
-    admin.add_view(ModelView(Empresa, db.session))
-    admin.add_view(ModelView(Alojamientos, db.session))
-    admin.add_view(ModelView(MediosPago, db.session))
-    admin.add_view(ModelView(Preguntas, db.session))
-    admin.add_view(ModelView(Transacciones, db.session))
-    admin.add_view(ModelView(QRDatabase, db.session))
-    admin.add_view(ModelView(Reservas, db.session))
-    admin.add_view(ModelView(GuardiaCivil, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -4,7 +4,8 @@ import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
 
 import { Home } from "./pages/home";
-import { Login } from "./pages/login";
+import { Demo } from "./pages/demo";
+import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -25,7 +26,8 @@ const Layout = () => {
                     <Navbar />
                     <Routes>
                         <Route element={<Home />} path="/" />
-                        <Route element={<Login />} path="/login" />
+                        <Route element={<Demo />} path="/demo" />
+                        <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
                     <Footer />
