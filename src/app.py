52611a9e6515610dc3813f7e5ebeb06331898ebe
Alexader Quejada cordoba <quejadacordobaalexander@gmail.com>
2025-01-24 19:22:39 +0000

    3-rama actualizada con develop

--- src/app.py
+++ src/app.py
@@ -12,6 +12,7 @@ from api.admin import setup_admin
 from api.commands import setup_commands
 from flask_jwt_extended import JWTManager
 from flask_babel import Babel
+from flask_babel import Babel
 
 
 # from models import Person
